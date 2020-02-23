8b7a7affd6a969e3750fc027442c1c77c59a00a4
tonie <tonie@tonie.com>
2020-02-23 22:20:29 +0100

    MyPortfolio

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+    <!--Bootstrap Framwork-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Body of portfolio-->
+    <link href="{{asset('css/portfolioBody.css')}}" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+
+
+    <div class="container-fluid">
+        <div class="content">
+            <!--laravel code-->
+            <div class="title">
+                <!--Bootstrap Code-->
+                <div class="alert" role="alert">
+
+                    My Introduction Begins Here
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="jumbotron jbT">
+        <div class="page-header">
+            <div class="txtCollection">
+                <h1>Othman Adi :
+                    <br>
+                    <small>
+                        i am a self tough Programmer/Music Producer From Hama,Syria
+                    </small>
+                </h1>
+                <h3>My mission in life is to Create and help
+                </h3>
+                <br>
+                ------
+                <br>
+                ------
+                <br>
+                ------
+                <br>
+                ------
+            </div>
+        </div>
+    </div>
+
+
+    <div class="dropdown-divider"></div>
+
+    <!--CV-->
+    <div class="card">
+        <!--My curriculum vitae-->
+        <div class="cv">
+            <div class="containFirstpic">
+                <img class="firstPic" src="/images/cv.png" style="height:1550px ; width:550px">
+            </div>
+            <!--My curriculum vitae-->
+            <img class="secondPic" src="/images/cvII.png" style="height:1550px ; width:450px">
+        </div>
+        <div class="card-body">
+            <div class="card-title">
+                hope you enjoyed Reading!
+                <a href="#" class="btn btn-light">
+                    Email me
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!--Empty Links to be used-->
+    <div class="links">
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+    </div>
+
+    <footer class="page-footer">
+
+        <div class="footer-copyright text-center py-3">@2020 copyright
+    </footer>
+
+</body>
+
+</html>
